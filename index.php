4f9444f0088ddc33597baf2deb3516f8fe0cc455
volter9 <volter925@gmail.com>
2015-10-17 21:20:31 -0700

    Remove braces

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
  * 
  * @author volter9
  * @license WTFPL 2.0
- * @version 0.1
+ * @version 0.1.1
  */
 
 $route = trim(!empty($_GET['route']) ? $_GET['route'] : 'index', '/');
@@ -13,7 +13,7 @@ $route = trim(!empty($_GET['route']) ? $_GET['route'] : 'index', '/');
 $file = str_replace('..', '', "content/$route");
 $file = file_exists("$file.php") ? "$file.php" : 'content/404.php';
 
-file_exists('theme/functions.php') and (require 'theme/functions.php');
+file_exists('theme/functions.php') and require 'theme/functions.php';
 
 ob_start() and $data = require $file;
 $content = ob_get_clean();
